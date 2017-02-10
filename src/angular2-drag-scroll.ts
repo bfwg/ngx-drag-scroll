061d1aa173da16b158df57f18b8a561f2ac68678
Fan Jin <fanjin1989@gmail.com>
2017-02-09 22:01:13 -0800

    Implement hide scrollbar feature

--- src/angular2-drag-scroll.ts
+++ src/angular2-drag-scroll.ts
@@ -7,15 +7,15 @@ import {
   Input,
   OnInit,
   OnChanges,
-  HostListener,
-  SimpleChange
+  AfterViewChecked,
+  HostListener
 } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 @Directive({
   selector: '[drag-scroll]'
 })
-export class DragScroll implements OnDestroy, OnInit, OnChanges {
+export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecked {
 
   private _scrollbarHidden: boolean;
 
@@ -45,29 +45,33 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges {
    */
   rect: ClientRect;
 
+  parentNode: HTMLElement;
+
+  wrapper: HTMLDivElement;
+
   /**
    * Whether the scrollbar is hidden
    */
-  @Input('hide-scrollbar')
+  @Input('scrollbar-hidden')
   get scrollbarHidden() { return this._scrollbarHidden; }
   set scrollbarHidden(value: boolean) { this._scrollbarHidden = value; };
 
   /**
-   * Whether horizontally and vertically draging and scrolling events will be disabled
+   * Whether horizontally and vertically draging and scrolling is be disabled
    */
   @Input('drag-scroll-disabled')
   get disabled() { return this._disabled; }
   set disabled(value: boolean) { this._disabled = value; };
 
   /**
-   * Whether horizontally dragging and scrolling events will be disabled
+   * Whether horizontally dragging and scrolling is be disabled
    */
   @Input('drag-scroll-x-disabled')
   get xDisabled() { return this._xDisabled; }
   set xDisabled(value: boolean) { this._xDisabled = value; };
 
   /**
-   * Whether vertically dragging and scrolling events will be disabled
+   * Whether vertically dragging and scrolling events is disabled
    */
   @Input('drag-scroll-y-disabled')
   get yDisabled() { return this._yDisabled; }
@@ -88,14 +92,13 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges {
   constructor(
     private el: ElementRef, private renderer: Renderer
   ) {
-    el.nativeElement.style.overflow = 'scroll';
+    el.nativeElement.style.overflow = 'auto';
     el.nativeElement.style.whiteSpace = 'noWrap';
     document.addEventListener('mousemove', this.onMouseMoveHandler, false);
     document.addEventListener('mouseup', this.onMouseUpHandler, false);
   }
 
-  ngOnChanges(changes: { [propertyName: string]: SimpleChange }) {
-
+  ngOnChanges() {
     if (this.scrollbarHidden) {
       this.hideScrollbar();
     } else {
@@ -120,6 +123,13 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges {
     this.renderer.setElementAttribute(this.el.nativeElement, 'drag-scroll', 'true');
   }
 
+  ngAfterViewChecked() {
+    // avoid extra ckecks
+    if (this.wrapper) {
+      this.checkScrollbar();
+    }
+  }
+
   ngOnDestroy() {
     this.renderer.setElementAttribute(this.el.nativeElement, 'drag-scroll', 'false');
     document.removeEventListener('mousemove', this.onMouseMoveHandler, false);
@@ -131,20 +141,16 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges {
       e.preventDefault();
       // Drag X
       if (!this.xDisabled) {
-        let rectRight = this.rect.left + this.el.nativeElement.offsetWidth;
-        let offsetX = e.pageX > rectRight ? e.pageX - rectRight :
-          e.pageX < this.rect.left ? e.pageX - this.rect.left : 0;
-        this.el.nativeElement.scrollLeft = this.el.nativeElement.scrollLeft - e.clientX + this.downX - offsetX;
-        this.downX = e.clientX + offsetX;
+        this.el.nativeElement.scrollLeft =
+          this.el.nativeElement.scrollLeft - e.clientX + this.downX;
+        this.downX = e.clientX;
       }
 
       // Drag Y
       if (!this.yDisabled) {
-        let rectBottom = this.rect.top + this.el.nativeElement.offsetHeight;
-        let offsetY = e.pageY > rectBottom ? e.pageY - rectBottom :
-          e.pageY < this.rect.top ? e.pageY - this.rect.top : 0;
-        this.el.nativeElement.scrollTop = this.el.nativeElement.scrollTop - e.clientY + this.downY - offsetY;
-        this.downY = e.clientY + offsetY;
+        this.el.nativeElement.scrollTop =
+          this.el.nativeElement.scrollTop - e.clientY + this.downY;
+        this.downY = e.clientY;
       }
     }
     return false;
@@ -167,10 +173,11 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges {
   private hideScrollbar(): void {
     this.parentNode = this.el.nativeElement.parentNode;
     this.wrapper = document.createElement('div');
-    this.wrapper.style.width = this.el.nativeElement.style.width;
-    this.wrapper.style.height = this.el.nativeElement.style.height;
+    this.wrapper.style.width = this.el.nativeElement.offsetWidth + 'px';
+    this.wrapper.style.height = this.el.nativeElement.offsetHeight + 'px';
     this.wrapper.style.overflow = 'hidden';
-    this.el.nativeElement.style.width = this.el.nativeElement.style.height = 'calc(100% + 20px)';
+    this.el.nativeElement.style.width = 'calc(100% + 20px)';
+    this.el.nativeElement.style.height = 'calc(100% + 20px)';
     // set the wrapper as child (instead of the element)
     this.parentNode.replaceChild(this.wrapper, this.el.nativeElement);
     // set element as child of wrapper
@@ -178,10 +185,26 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges {
   }
 
   private showScrollbar(): void {
-    this.el.nativeElement.style.width = this.wrapper.style.width;
-    this.el.nativeElement.style.height = this.wrapper.style.height;
-    this.parentNode.removeChild(this.wrapper);
-    this.parentNode.appendChild(this.el.nativeElement);
+    if (this.wrapper) {
+      this.el.nativeElement.style.width = this.wrapper.style.width;
+      this.el.nativeElement.style.height = this.wrapper.style.height;
+      this.parentNode.removeChild(this.wrapper);
+      this.parentNode.appendChild(this.el.nativeElement);
+      this.wrapper = null;
+    }
+  }
+
+  private checkScrollbar() {
+    if (this.el.nativeElement.scrollWidth <= this.el.nativeElement.clientWidth) {
+      this.el.nativeElement.style.height = '100%';
+    } else {
+      this.el.nativeElement.style.height = 'calc(100% + 20px)';
+    }
+    if (this.el.nativeElement.scrollHeight <= this.el.nativeElement.clientHeight) {
+      this.el.nativeElement.style.width = '100%';
+    } else {
+      this.el.nativeElement.style.width = 'calc(100% + 20px)';
+    }
   }
 }
 
