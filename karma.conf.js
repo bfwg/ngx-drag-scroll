ed087cf0aa652468822226a223f0116d3712696b
Fan Jin <fanjin1989@gmail.com>
2017-06-26 10:28:44 -0700

    fix travis

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,13 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    customLaunchers: {
+      Chrome_travis_ci: {
+          base: 'Chrome',
+          flags: ['--no-sandbox']
+      }
+    }
   };
   if(process.env.TRAVIS){
     configuration.browsers = ['Chrome_travis_ci'];
