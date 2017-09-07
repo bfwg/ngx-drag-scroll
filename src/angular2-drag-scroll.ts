bae20a03849ca51f675dac510eb77c70b85d93fb
Fan Jin <fanjin1989@gmail.com>
2017-09-07 14:24:31 -0700

    improve nav buttons
    - add ablitiy to snap item when navigating.
    - add ablitiy to snap item when scrolling.
    - add ablitiy to snap item when draging.

--- src/angular2-drag-scroll.ts
+++ src/angular2-drag-scroll.ts
@@ -5,9 +5,11 @@ import {
   Renderer,
   OnDestroy,
   Input,
+  Output,
   OnInit,
   OnChanges,
   AfterViewChecked,
+  EventEmitter,
   HostListener
 } from '@angular/core';
 import { DragScrollOption } from './interface/drag-scroll-option';
@@ -24,12 +26,17 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecke
   private _xDisabled: boolean;
 
   private _yDisabled: boolean;
-
-  private _nav: boolean;
   /**
    * Is the user currently pressing the element
    */
   isPressed = false;
+
+  /**
+   * Is the user currently scrolling the element
+   */
+  isScrolling = false;
+
+  scrollTimer;
 
   /**
    * The x coordinates on the element
@@ -56,28 +63,21 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecke
 
   onMouseMoveHandler = this.onMouseMove.bind(this);
   onMouseDownHandler = this.onMouseDown.bind(this);
+  onScrollHandler = this.onScroll.bind(this);
   onMouseUpHandler = this.onMouseUp.bind(this);
 
   mouseMoveListener: Function;
   mouseDownListener: Function;
+  scrollListener: Function;
   mouseUpListener: Function;
-  nextMouseUpListener: Function;
-  prevMouseUpListener: Function;
-  nextTouchEndListener: Function;
-  prevTouchEndListener: Function;
+
+  currIndex = 0;
+
+  isAnimating = false;
+
+  scrollReachesRightEnd = false;
 
 
-  /*
-   * Nav buttons
-   */
-  prevBtn = document.createElement('button');
-  nextBtn = document.createElement('button');
-  prevBtnDownListener: Function;
-  prevTouchStartListener: Function;
-  nextBtnDownListener: Function;
-  nextTouchStartListener: Function;
-  navBtnDiv: HTMLElement;
-  navInterval: number;
 
   /**
    * Whether the scrollbar is hidden
@@ -107,14 +107,15 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecke
   get yDisabled() { return this._yDisabled; }
   set yDisabled(value: boolean) { this._yDisabled = value; };
 
-  @Input('drag-scroll-nav')
-  get nav() { return this._nav; }
-  set nav(value: boolean) { this._nav = value; };
-
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.setScrollBar(true);
+    this.resetScrollLocation();
   }
+
+  @Output('leftBound') reachesLeftBound = new EventEmitter<boolean>();
+  @Output('rightBound') reachesRightBound = new EventEmitter<boolean>();
+
 
   constructor(
     private el: ElementRef, private renderer: Renderer
@@ -123,22 +124,23 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecke
     el.nativeElement.style.overflow = 'auto';
     el.nativeElement.style.whiteSpace = 'noWrap';
 
-    this.mouseMoveListener = renderer.listenGlobal('document', 'mousemove', this.onMouseMoveHandler);
     this.mouseDownListener = renderer.listenGlobal(el.nativeElement, 'mousedown', this.onMouseDownHandler);
+    this.scrollListener = renderer.listenGlobal(el.nativeElement, 'scroll', this.onScrollHandler);
+    this.mouseMoveListener = renderer.listenGlobal('document', 'mousemove', this.onMouseMoveHandler);
     this.mouseUpListener = renderer.listenGlobal('document', 'mouseup', this.onMouseUpHandler);
   }
 
-  public attach({disabled, scrollbarHidden, yDisabled, xDisabled, nav}: DragScrollOption): void {
+  public attach({disabled, scrollbarHidden, yDisabled, xDisabled}: DragScrollOption): void {
     this.disabled = disabled;
     this.scrollbarHidden = scrollbarHidden;
     this.yDisabled = yDisabled;
     this.xDisabled = xDisabled;
-    this.nav = nav;
     this.ngOnChanges();
   }
 
   ngOnChanges() {
     this.setScrollBar();
+    this.resetScrollLocation();
 
     if (this.xDisabled || this.disabled) {
       this.disableScroll('x');
@@ -152,18 +154,13 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecke
       this.enableScroll('y');
     }
 
-    if (this.nav) {
-      this.showNavButton();
-    } else {
-      this.hideNavButton();
-    }
-
   }
 
   ngOnInit(): void {
     // auto assign computed css
     this.displayType = window.getComputedStyle(this.el.nativeElement).display;
     this.el.nativeElement.style.display = this.displayType;
+
     this.rect = this.el.nativeElement.getBoundingClientRect();
     this.renderer.setElementAttribute(this.el.nativeElement, 'drag-scroll', 'true');
   }
@@ -179,7 +176,6 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecke
     this.renderer.setElementAttribute(this.el.nativeElement, 'drag-scroll', 'false');
     this.mouseMoveListener();
     this.mouseUpListener();
-    this.hideNavButton();
   }
 
   onMouseMove(e: MouseEvent) {
@@ -202,15 +198,38 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecke
     return false;
   }
 
+
   onMouseDown(e: MouseEvent) {
     this.isPressed = true;
     this.downX = e.clientX;
     this.downY = e.clientY;
   }
 
+  onScroll(e) {
+    const ele = this.el.nativeElement;
+    if ((ele.scrollLeft + ele.offsetWidth) >= ele.scrollWidth) {
+      this.scrollReachesRightEnd = true;
+    } else {
+      this.scrollReachesRightEnd = false;
+    }
+    // if (!this.isPressed && !this.isAnimating) {
+    if (!this.isPressed && !this.isAnimating) {
+      this.isScrolling = true;
+      clearTimeout(this.scrollTimer);
+      this.scrollTimer = setTimeout(() => {
+        this.isScrolling = false;
+        this.snapToCurrentIndex();
+      }, 500);
+    }
+    // }
+  }
+
   onMouseUp(e: MouseEvent) {
     e.preventDefault();
-    this.isPressed = false;
+    if (this.isPressed) {
+      this.isPressed = false;
+      this.snapToCurrentIndex();
+    }
     return false;
   }
 
@@ -257,6 +276,8 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecke
     if (this.el.nativeElement.scrollHeight <= this.el.nativeElement.clientHeight) {
       this.el.nativeElement.style.width = '100%';
     } else {
+      let lastChild = this.el.nativeElement.children[this.el.nativeElement.children.length - 1];
+      lastChild.style['margin-right'] = this.scrollbarWidth;
       this.el.nativeElement.style.width = `calc(100% + ${this.scrollbarWidth})`;
     }
   }
@@ -314,90 +335,135 @@ export class DragScroll implements OnDestroy, OnInit, OnChanges, AfterViewChecke
   /*
    * Nav button
    */
+  moveLeft() {
+    const childrenArr = this.el.nativeElement.children;
+    const ele = this.el.nativeElement;
+    if (this.currIndex !== 0) {
+      // reach left most
+      const scrollTo = ele.scrollLeft - childrenArr[this.currIndex - 1].clientWidth;
+      this.scrollTo(ele, scrollTo, 500, () => {
+        this.currIndex--;
+      });
+    }
+  }
 
-  showNavButton() {
-    this.navBtnDiv = document.createElement('div');
-    this.navBtnDiv.style.position = 'absolute';
-    this.navBtnDiv.style.left = '50%';
-    this.navBtnDiv.style.transform = 'translate(-50%, 0)';
+  moveRight() {
+    const childrenArr = this.el.nativeElement.children;
+    const ele = this.el.nativeElement;
+    if (!this.scrollReachesRightEnd) {
+      const scrollTo = ele.scrollLeft + childrenArr[this.currIndex].clientWidth;
+      this.scrollTo(ele, scrollTo, 500, () => {
+        this.currIndex++;
+      });
+    }
+  }
 
-    this.prevBtn.innerHTML = 'prev';
-    this.prevBtn.style['-moz-user-select'] = 'none';
-    this.prevBtn.style['-webkit-user-select'] = 'none';
+  /*
+  * The below solution is heavily inspired from
+  * https://gist.github.com/andjosh/6764939
+  */
+  private scrollTo(element, to, duration, animationFinished?) {
+    //t = current time
+    //b = start value
+    //c = change in value
+    //d = duration
+    let self = this;
+    if (!self.isAnimating) {
+      self.isAnimating = true;
+      let start = element.scrollLeft,
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
 
-    this.nextBtn.innerHTML = 'next';
-    this.nextBtn.style['-moz-user-select'] = 'none';
-    this.nextBtn.style['-webkit-user-select'] = 'none';
+      let animateScroll = function() {
+        currentTime += increment;
+        let val = easeInOutQuad(currentTime, start, change, duration);
+        element.scrollLeft = val;
+        if(currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        } else {
+          // run one more frame to make sure the animation is fully finished
+          setTimeout(() => {
+            self.isAnimating = false;
+            if (animationFinished) {
+              animationFinished();
+            }
+            self.setNavStatus();
+          }, increment);
+        }
+      };
 
-    if (this.mobileAndTabletcheck()) {
-      this.registerNavMobileListeners();
+      let easeInOutQuad = function (t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+      };
+      animateScroll();
+    }
+  }
+
+  private snapToCurrentIndex() {
+    const childrenArr = this.el.nativeElement.children;
+    let childrenWidth = 0;
+    const ele = this.el.nativeElement;
+    for (let i = 0; i < childrenArr.length; i++) {
+      if (i === childrenArr.length - 1) {
+        this.currIndex = childrenArr.length;
+        break;
+      }
+
+      const nextChildrenWidth = childrenWidth + childrenArr[i + 1].clientWidth;
+
+      const currentClildWidth = childrenArr[i].clientWidth;
+      const nextClildWidth = childrenArr[i + 1].clientWidth;
+
+      if (ele.scrollLeft >= childrenWidth &&
+          ele.scrollLeft <= nextChildrenWidth) {
+
+        if (nextChildrenWidth - ele.scrollLeft > currentClildWidth / 2) {
+          // roll back scrolling
+          this.currIndex = i;
+          this.scrollTo(ele, childrenWidth, 500);
+        } else {
+          // forward scrolling
+          this.currIndex = i + 1;
+          this.scrollTo(ele, childrenWidth + currentClildWidth, 500);
+        }
+        break;
+
+      }
+      childrenWidth += childrenArr[i].clientWidth;
+    }
+  }
+
+  private setNavStatus() {
+    const childrenArr = this.el.nativeElement.children;
+    const ele = this.el.nativeElement;
+    if (childrenArr.length <= 1) {
+      // only one element
+      this.reachesLeftBound.emit(true);
+      this.reachesRightBound.emit(true);
+    } else if (this.scrollReachesRightEnd) {
+      // reached right end
+      this.reachesLeftBound.emit(false);
+      this.reachesRightBound.emit(true);
+    } else if (this.currIndex === 0) {
+      // reached left end
+      this.reachesRightBound.emit(false);
+      this.reachesLeftBound.emit(true);
     } else {
-      this.registerNavBrowserListeners();
-    }
-
-    this.navBtnDiv.appendChild(this.prevBtn);
-    this.navBtnDiv.appendChild(this.nextBtn);
-    this.el.nativeElement.appendChild(this.navBtnDiv);
-  }
-
-  moveLeft(speed: number, stop?: boolean) {
-    this.navInterval = window.setInterval(() => {
-      this.el.nativeElement.scrollLeft -= 8;
-    }, speed);
-  }
-
-  moveRight(speed: number, stop?: boolean) {
-    this.navInterval = window.setInterval(() => {
-      this.el.nativeElement.scrollLeft += 8;
-    }, speed);
-  }
-
-  stopNavMov() {
-    clearInterval(this.navInterval);
-    this.navInterval = null;
-  }
-
-  hideNavButton() {
-    if (this.navBtnDiv) {
-      this.el.nativeElement.parentNode.removeChild(this.navBtnDiv);
-      this.nextBtnDownListener();
-      this.prevBtnDownListener();
-      this.nextMouseUpListener();
-      this.prevMouseUpListener();
-      this.prevTouchStartListener();
-      this.nextTouchStartListener();
-      this.prevTouchEndListener();
-      this.nextTouchEndListener();
+      // in the middle
+      this.reachesLeftBound.emit(false);
+      this.reachesRightBound.emit(false);
     }
   }
 
-  registerNavBrowserListeners() {
-    this.nextBtnDownListener = this.renderer.listen(this.nextBtn, 'mousedown', () => {
-      this.moveRight(10);
-    });
-    this.prevBtnDownListener = this.renderer.listen(this.prevBtn, 'mousedown', () => {
-      this.moveLeft(10);
-    });
-    this.nextMouseUpListener = this.renderer.listen(this.nextBtn, 'mouseup', this.stopNavMov.bind(this));
-    this.prevMouseUpListener = this.renderer.listen(this.prevBtn, 'mouseup', this.stopNavMov.bind(this));
+  private resetScrollLocation() {
+    const ele = this.el.nativeElement;
+    this.scrollTo(ele, 0, 0);
+    this.currIndex = 0;
   }
-
-  registerNavMobileListeners() {
-    this.nextTouchStartListener = this.renderer.listen(this.nextBtn, 'touchstart', () => {
-      this.moveRight(10);
-    });
-    this.prevTouchStartListener = this.renderer.listen(this.prevBtn, 'touchstart', () => {
-      this.moveLeft(10);
-    });
-    this.nextTouchEndListener = this.renderer.listen(this.nextBtn, 'touchend', this.stopNavMov.bind(this));
-    this.prevTouchEndListener = this.renderer.listen(this.prevBtn, 'touchend', this.stopNavMov.bind(this));
-  }
-
-  mobileAndTabletcheck() {
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window['opera']);
-    return check;
-  };
 }
 
 @NgModule({
