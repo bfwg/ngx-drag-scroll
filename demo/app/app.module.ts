1fdc1f02706368e4c886711abc6c2e0f3b0f2cd8
Karthik K <oss.akk@gmail.com>
2017-11-11 01:28:25 +0530

    httpmodule is getting deprecated. remove it in favor of httpclientmodule (#71)

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { MatCommonModule, MatIconModule, MatSlideToggleModule, MatToolbarModule,
   MatButtonModule } from '@angular/material';
 
@@ -33,7 +33,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     DragScrollModule,
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     MatCommonModule,
     MatIconModule,
     MatSlideToggleModule,
