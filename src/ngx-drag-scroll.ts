7d073e6abdeb02c9f6f36debc64cfd84f95529a0
Karthik Kumar <akkumar@gmail.com>
2017-12-26 23:01:12 +0530

    refactor: fix tslint errors

--- src/ngx-drag-scroll.ts
+++ src/ngx-drag-scroll.ts
@@ -19,10 +19,6 @@ import { DragScrollOption } from './interface/drag-scroll-option';
 })
 export class DragScrollDirective implements OnDestroy, OnInit, OnChanges, DoCheck {
 
-  @Output() reachesLeftBound = new EventEmitter<boolean>();
-
-  @Output() reachesRightBound = new EventEmitter<boolean>();
-
   private _scrollbarHidden: boolean;
 
   private _disabled: boolean;
@@ -89,6 +85,216 @@ export class DragScrollDirective implements OnDestroy, OnInit, OnChanges, DoChec
   prevChildrenLength = 0;
 
   childrenArr: Array<Element> = [];
+
+  @Output() reachesLeftBound = new EventEmitter<boolean>();
+
+  @Output() reachesRightBound = new EventEmitter<boolean>();
+
+
+  private disableScroll(axis: string): void {
+    this.el.nativeElement.style[`overflow-${axis}`] = 'hidden';
+  }
+
+  private enableScroll(axis: string): void {
+    this.el.nativeElement.style[`overflow-${axis}`] = 'auto';
+  }
+
+  private hideScrollbar(): void {
+    if (this.el.nativeElement.style.display !== 'none' && !this.wrapper) {
+      this.parentNode = this.el.nativeElement.parentNode;
+
+      // clone
+      this.wrapper = this.el.nativeElement.cloneNode(true);
+      // remove all children
+      if (this.wrapper !== null) {
+        while (this.wrapper.hasChildNodes()) {
+          if (this.wrapper.lastChild !== null) {
+            this.wrapper.removeChild(this.wrapper.lastChild);
+          }
+        }
+        this.wrapper.style.overflow = 'hidden';
+
+        this.el.nativeElement.style.width = `calc(100% + ${this.scrollbarWidth})`;
+        this.el.nativeElement.style.height = `calc(100% + ${this.scrollbarWidth})`;
+        // set the wrapper as child (instead of the element)
+        this.parentNode.replaceChild(this.wrapper, this.el.nativeElement);
+        // set element as child of wrapper
+        this.wrapper.appendChild(this.el.nativeElement);
+      }
+    }
+  }
+
+  private showScrollbar(): void {
+    if (this.wrapper) {
+      this.el.nativeElement.style.width = this.elWidth;
+      this.el.nativeElement.style.height = this.elHeight;
+      this.parentNode.removeChild(this.wrapper);
+      this.parentNode.appendChild(this.el.nativeElement);
+      this.wrapper = null;
+    }
+  }
+
+  private checkScrollbar() {
+    if (this.el.nativeElement.scrollWidth <= this.el.nativeElement.clientWidth) {
+      this.el.nativeElement.style.height = '100%';
+    } else {
+      this.el.nativeElement.style.height = `calc(100% + ${this.scrollbarWidth})`;
+    }
+    if (this.el.nativeElement.scrollHeight <= this.el.nativeElement.clientHeight) {
+      this.el.nativeElement.style.width = '100%';
+    } else {
+      this.el.nativeElement.style.width = `calc(100% + ${this.scrollbarWidth})`;
+    }
+  }
+
+  private setScrollBar(): void {
+    if (this.scrollbarHidden) {
+      this.hideScrollbar();
+    } else {
+      this.showScrollbar();
+    }
+  }
+
+  private getScrollbarWidth(): number {
+    /**
+     * Browser Scrollbar Widths (2016)
+     * OSX (Chrome, Safari, Firefox) - 15px
+     * Windows XP (IE7, Chrome, Firefox) - 17px
+     * Windows 7 (IE10, IE11, Chrome, Firefox) - 17px
+     * Windows 8.1 (IE11, Chrome, Firefox) - 17px
+     * Windows 10 (IE11, Chrome, Firefox) - 17px
+     * Windows 10 (Edge 12/13) - 12px
+     */
+    let widthNoScroll = 0;
+    let widthWithScroll = 0;
+    const outer: HTMLDivElement | null = document.createElement('div');
+    if (outer !== null) {
+      outer.style.visibility = 'hidden';
+      outer.style.width = '100px';
+      outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+
+      document.body.appendChild(outer);
+
+      widthNoScroll = outer.offsetWidth;
+      // force scrollbars
+      outer.style.overflow = 'scroll';
+
+      // add innerdiv
+      const inner = document.createElement('div');
+      inner.style.width = '100%';
+      outer.appendChild(inner);
+
+      widthWithScroll = inner.offsetWidth;
+
+      // remove divs
+      if (outer.parentNode !== null) {
+        outer.parentNode.removeChild(outer);
+      }
+    }
+    /**
+     * Scrollbar width will be 0 on Mac OS with the
+     * default "Only show scrollbars when scrolling" setting (Yosemite and up).
+     * setting defult with to 20;
+     */
+    return widthNoScroll - widthWithScroll || 20;
+  }
+
+  /*
+  * The below solution is heavily inspired from
+  * https://gist.github.com/andjosh/6764939
+  */
+  private scrollTo(element: Element, to: number, duration: number) {
+    const self = this;
+    self.isAnimating = true;
+    const start = element.scrollLeft,
+      change = to - start,
+      increment = 20;
+    let currentTime = 0;
+
+    // t = current time
+    // b = start value
+    // c = change in value
+    // d = duration
+    const easeInOutQuad = function (t: number, b: number, c: number, d: number) {
+      t /= d / 2;
+      if (t < 1) {
+        return c / 2 * t * t + b;
+      }
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    };
+
+    const animateScroll = function() {
+      currentTime += increment;
+      element.scrollLeft = easeInOutQuad(currentTime, start, change, duration);
+      if (currentTime < duration) {
+          self.scrollToTimer = window.setTimeout(animateScroll, increment);
+      } else {
+        // run one more frame to make sure the animation is fully finished
+        setTimeout(() => {
+          self.isAnimating = false;
+        }, increment);
+      }
+    };
+    animateScroll();
+  }
+
+  private snapToCurrentIndex() {
+    let childrenWidth = 0;
+    const ele = this.el.nativeElement;
+    for (let i = 0; i < this.childrenArr.length; i++) {
+      if (i === this.childrenArr.length - 1) {
+        this.currIndex = this.childrenArr.length;
+        break;
+      }
+
+      const nextChildrenWidth = childrenWidth + this.childrenArr[i + 1].clientWidth;
+
+      const currentClildWidth = this.childrenArr[i].clientWidth;
+      const nextClildWidth = this.childrenArr[i + 1].clientWidth;
+
+      if (ele.scrollLeft >= childrenWidth &&
+          ele.scrollLeft <= nextChildrenWidth) {
+
+        if (nextChildrenWidth - ele.scrollLeft > currentClildWidth / 2 && !this.scrollReachesRightEnd) {
+          // roll back scrolling
+          this.currIndex = i;
+          this.scrollTo(ele, childrenWidth, 500);
+        } else {
+          // forward scrolling
+          this.currIndex = i + 1;
+          this.scrollTo(ele, childrenWidth + currentClildWidth, 500);
+        }
+        break;
+
+      }
+      childrenWidth += this.childrenArr[i].clientWidth;
+    }
+  }
+
+  private toChildrenLocation(): number {
+    let to = 0;
+    for (let i = 0; i < this.currIndex; i++) {
+      to += this.childrenArr[i].clientWidth;
+    }
+    return to;
+  }
+
+  private resetScrollLocation() {
+    const ele = this.el.nativeElement;
+    this.scrollTo(ele, 0, 0);
+    this.currIndex = 0;
+  }
+
+  private markElDimension() {
+    if (this.wrapper) {
+      this.elWidth = this.wrapper.style.width;
+      this.elHeight = this.wrapper.style.height;
+    } else {
+      this.elWidth = this.el.nativeElement.style.width;
+      this.elHeight = this.el.nativeElement.style.height;
+    }
+  }
 
   /**
    * Whether the scrollbar is hidden
@@ -259,115 +465,6 @@ export class DragScrollDirective implements OnDestroy, OnInit, OnChanges, DoChec
     return false;
   }
 
-  private disableScroll(axis: string): void {
-    this.el.nativeElement.style[`overflow-${axis}`] = 'hidden';
-  }
-
-  private enableScroll(axis: string): void {
-    this.el.nativeElement.style[`overflow-${axis}`] = 'auto';
-  }
-
-  private hideScrollbar(): void {
-    if (this.el.nativeElement.style.display !== 'none' && !this.wrapper) {
-      this.parentNode = this.el.nativeElement.parentNode;
-
-      // clone
-      this.wrapper = this.el.nativeElement.cloneNode(true);
-      // remove all children
-      if (this.wrapper !== null) {
-        while (this.wrapper.hasChildNodes()) {
-          if (this.wrapper.lastChild !== null) {
-            this.wrapper.removeChild(this.wrapper.lastChild);
-          }
-        }
-        this.wrapper.style.overflow = 'hidden';
-
-        this.el.nativeElement.style.width = `calc(100% + ${this.scrollbarWidth})`;
-        this.el.nativeElement.style.height = `calc(100% + ${this.scrollbarWidth})`;
-        // set the wrapper as child (instead of the element)
-        this.parentNode.replaceChild(this.wrapper, this.el.nativeElement);
-        // set element as child of wrapper
-        this.wrapper.appendChild(this.el.nativeElement);
-      }
-    }
-  }
-
-  private showScrollbar(): void {
-    if (this.wrapper) {
-      this.el.nativeElement.style.width = this.elWidth;
-      this.el.nativeElement.style.height = this.elHeight;
-      this.parentNode.removeChild(this.wrapper);
-      this.parentNode.appendChild(this.el.nativeElement);
-      this.wrapper = null;
-    }
-  }
-
-  private checkScrollbar() {
-    if (this.el.nativeElement.scrollWidth <= this.el.nativeElement.clientWidth) {
-      this.el.nativeElement.style.height = '100%';
-    } else {
-      this.el.nativeElement.style.height = `calc(100% + ${this.scrollbarWidth})`;
-    }
-    if (this.el.nativeElement.scrollHeight <= this.el.nativeElement.clientHeight) {
-      this.el.nativeElement.style.width = '100%';
-    } else {
-      this.el.nativeElement.style.width = `calc(100% + ${this.scrollbarWidth})`;
-    }
-  }
-
-  private setScrollBar(): void {
-    if (this.scrollbarHidden) {
-      this.hideScrollbar();
-    } else {
-      this.showScrollbar();
-    }
-  }
-
-  private getScrollbarWidth(): number {
-    /**
-     * Browser Scrollbar Widths (2016)
-     * OSX (Chrome, Safari, Firefox) - 15px
-     * Windows XP (IE7, Chrome, Firefox) - 17px
-     * Windows 7 (IE10, IE11, Chrome, Firefox) - 17px
-     * Windows 8.1 (IE11, Chrome, Firefox) - 17px
-     * Windows 10 (IE11, Chrome, Firefox) - 17px
-     * Windows 10 (Edge 12/13) - 12px
-     */
-    let widthNoScroll = 0;
-    let widthWithScroll = 0;
-    const outer: HTMLDivElement | null = document.createElement('div');
-    if (outer !== null) {
-      outer.style.visibility = 'hidden';
-      outer.style.width = '100px';
-      outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
-
-      document.body.appendChild(outer);
-
-      widthNoScroll = outer.offsetWidth;
-      // force scrollbars
-      outer.style.overflow = 'scroll';
-
-      // add innerdiv
-      const inner = document.createElement('div');
-      inner.style.width = '100%';
-      outer.appendChild(inner);
-
-      widthWithScroll = inner.offsetWidth;
-
-      // remove divs
-      if (outer.parentNode !== null) {
-        outer.parentNode.removeChild(outer);
-      }
-    }
-    /**
-     * Scrollbar width will be 0 on Mac OS with the
-     * default "Only show scrollbars when scrolling" setting (Yosemite and up).
-     * setting defult with to 20;
-     */
-    return widthNoScroll - widthWithScroll || 20;
-  }
-
-
   /*
    * Nav button
    */
@@ -425,102 +522,7 @@ export class DragScrollDirective implements OnDestroy, OnInit, OnChanges, DoChec
     }
   }
 
-  /*
-  * The below solution is heavily inspired from
-  * https://gist.github.com/andjosh/6764939
-  */
-  private scrollTo(element: Element, to: number, duration: number) {
-    const self = this;
-    self.isAnimating = true;
-    const start = element.scrollLeft,
-      change = to - start,
-      increment = 20;
-    let currentTime = 0;
 
-    // t = current time
-    // b = start value
-    // c = change in value
-    // d = duration
-    const easeInOutQuad = function (t: number, b: number, c: number, d: number) {
-      t /= d / 2;
-      if (t < 1) {
-        return c / 2 * t * t + b;
-      }
-      t--;
-      return -c / 2 * (t * (t - 2) - 1) + b;
-    };
-
-    const animateScroll = function() {
-      currentTime += increment;
-      element.scrollLeft = easeInOutQuad(currentTime, start, change, duration);
-      if (currentTime < duration) {
-          self.scrollToTimer = window.setTimeout(animateScroll, increment);
-      } else {
-        // run one more frame to make sure the animation is fully finished
-        setTimeout(() => {
-          self.isAnimating = false;
-        }, increment);
-      }
-    };
-    animateScroll();
-  }
-
-  private snapToCurrentIndex() {
-    let childrenWidth = 0;
-    const ele = this.el.nativeElement;
-    for (let i = 0; i < this.childrenArr.length; i++) {
-      if (i === this.childrenArr.length - 1) {
-        this.currIndex = this.childrenArr.length;
-        break;
-      }
-
-      const nextChildrenWidth = childrenWidth + this.childrenArr[i + 1].clientWidth;
-
-      const currentClildWidth = this.childrenArr[i].clientWidth;
-      const nextClildWidth = this.childrenArr[i + 1].clientWidth;
-
-      if (ele.scrollLeft >= childrenWidth &&
-          ele.scrollLeft <= nextChildrenWidth) {
-
-        if (nextChildrenWidth - ele.scrollLeft > currentClildWidth / 2 && !this.scrollReachesRightEnd) {
-          // roll back scrolling
-          this.currIndex = i;
-          this.scrollTo(ele, childrenWidth, 500);
-        } else {
-          // forward scrolling
-          this.currIndex = i + 1;
-          this.scrollTo(ele, childrenWidth + currentClildWidth, 500);
-        }
-        break;
-
-      }
-      childrenWidth += this.childrenArr[i].clientWidth;
-    }
-  }
-
-  private toChildrenLocation(): number {
-    let to = 0;
-    for (let i = 0; i < this.currIndex; i++) {
-      to += this.childrenArr[i].clientWidth;
-    }
-    return to;
-  }
-
-  private resetScrollLocation() {
-    const ele = this.el.nativeElement;
-    this.scrollTo(ele, 0, 0);
-    this.currIndex = 0;
-  }
-
-  private markElDimension() {
-    if (this.wrapper) {
-      this.elWidth = this.wrapper.style.width;
-      this.elHeight = this.wrapper.style.height;
-    } else {
-      this.elWidth = this.el.nativeElement.style.width;
-      this.elHeight = this.el.nativeElement.style.height;
-    }
-  }
 }
 
 @NgModule({
