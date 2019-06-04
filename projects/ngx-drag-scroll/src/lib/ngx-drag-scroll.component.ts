5a7acf4d854baf69b67c1a55d7b5fa4483646c4c
Fan Jin <fanjin1989@gmail.com>
2019-06-04 15:58:19 -0700

    fix: forward snap should not happen when the scroll left is 0

--- projects/ngx-drag-scroll/src/lib/ngx-drag-scroll.component.ts
+++ projects/ngx-drag-scroll/src/lib/ngx-drag-scroll.component.ts
@@ -28,6 +28,7 @@ import { DragScrollItemDirective } from './ngx-drag-scroll-item';
   `,
   styles: [`
     :host {
+      overflow: hidden;
       display: block;
     }
     .drag-scroll-content {
@@ -644,7 +645,7 @@ export class DragScrollComponent implements OnDestroy, AfterViewInit, OnChanges,
           if (snap) {
             this.scrollTo(this._contentRef.nativeElement, childrenWidth, this.snapDuration);
           }
-        } else {
+        } else if (this._contentRef.nativeElement.scrollLeft !== 0) {
           // forward scrolling
           if (!this.isAnimating) {
             this.currIndex = idx + 1;
